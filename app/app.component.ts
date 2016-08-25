4885ecae52d213ca3cd1e619f52eea56adad8f93
russ <russvetsper@gmail.com>
2016-08-25 13:32:57 -0700

    add app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from 'angular2/core';
+import { Skeleton } from './skeleton.model'
 
 @Component({
   selector: 'my-app',
+  directives:[],
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+  <div class="container">
+    <h1>Skeleton selection</h1>
+    <h2>Add New skeleton</h2>
+    <skeleton-list [skeletonList]="skeleton"></skeleton-list>
+  </div>
   `
 })
 
 export class AppComponent {
+public skeleton: Skeleton[];
+  constructor() {
+    this.skeleton = [
+      new Skeleton("Russ", "Big skeleton", 200),
+      new Skeleton("Yi", "Small skeleton", 100),
+      new Skeleton("Rouz", "Big skeleton", 160)
+    ];
+  }
+createSkeleton(SkeletonDetail:string[]):void{
+  var Skeleton: number = parseInt(SkeletonDetail[2]);
 
+
+ }
 }
---
+++ app/skeleton.model.ts
@@ -0,0 +1,5 @@
+export class Skeleton {
+  constructor (public name: string , public description: string, public weiht: number){
+
+  }
+}
